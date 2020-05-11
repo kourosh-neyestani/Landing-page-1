d5fec10b8c03765fc5d1d5669688872dc6c94134
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-11 23:30:24 +0430

    update hero sidebar

--- src/scenes/Home/Intro.js
+++ src/scenes/Home/Intro.js
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom";
 
 function Intro() {
     return (
-       <section></section>
+        <section className="section section-hero section-hero-1">
+            <div className="hero-content"></div>
+            <div className="hero-sidebar">
+                <div className="display-center">
+                    <div className="content">
+                        <h1>BUILD YOUR BODY STRONG</h1>
+                        <p>Lorem ipsum dolor sit amet consec tetur adipisicing elit.</p>
+                        <Link>
+                            <button className="button button-md button-light text-primary">About Me</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
