e8ca9beed001ddd3bfb75e04874c410edbf5f50e
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-11 18:59:10 +0430

    update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <header className="site-header">
+        <header className="app-header">
             <div className="header-logo">
                 <Link to={`/home-1`}>Mitra.</Link>
             </div>
