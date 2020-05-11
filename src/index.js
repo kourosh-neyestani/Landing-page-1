b427c4ef6970b24b27febe8dffc2faeddea68c3a
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-11 18:36:32 +0430

    add folder structure

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+
+// Scenes
+
+// Stylesheet
+import "./index.scss";
+
+const Root = () => {
+    return (
+        <BrowserRouter basename={"/"}>
+            <Switch>
+                {/* <Route exact path={`${process.env.PUBLIC_URL}/home-1`} component={Home} /> */}
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
+serviceWorker.unregister();
