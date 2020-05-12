4f737f8d1ddbb342432b82c5a68b102975dd21ba
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-12 19:17:13 +0430

    update header

--- src/scenes/Home/Intro.js
+++ src/scenes/Home/Intro.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Slider from "react-slick";
 import { Link } from "react-router-dom";
 
 function Intro() {
     return (
         <section className="section section-hero section-hero-1">
-            <div className="hero-content"></div>
+            <div className="hero-content">
+                <Slider className="el-slider">
+                    <div className="slide">
+                        <div className="image overlay-image" style={{backgroundImage: `url(/assets/images/hero/1-1.jpg)`}}></div>
+                    </div>
+                    <div className="slide">
+                        <div className="image overlay-image" style={{backgroundImage: `url(/assets/images/hero/1-2.jpg)`}}></div>
+                    </div>
+                    <div className="slide">
+                        <div className="image overlay-image" style={{backgroundImage: `url(/assets/images/hero/1-3.jpg)`}}></div>
+                    </div>
+                </Slider>
+            </div>
             <div className="hero-sidebar">
                 <div className="display-center">
                     <div className="content">
