31bac21b731a2abb79020a8fadbe694d1fe5f02d
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-12 12:10:51 +0430

    update about section

--- src/scenes/Home/About.js
+++ src/scenes/Home/About.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Container, Row, Col } from "react-grid-system";
 
 function About() {
     return (
-        <section class="section section-about section-about-1 pp-scrollable">
-            <div class="full-screen">
+        <section class="section section-about section-about-1">
+            <div class="display-full-screen">
                 <div class="display-center">
                     <div class="section-title-1">
-                        <div class="container">
+                        <Container>
                             <div class="my-avatar">
-                                <img src="assets/images/people/1.jpg" alt="My Avatar" />
+                                <img src="/assets/images/person/1.jpg" alt="My Avatar" />
                             </div>
                             <h2>I'am Miranda Smith.</h2>
                             <h3>Your new Yoga Coach.</h3>
@@ -16,60 +17,66 @@ function About() {
                             <button type="button" class="button button-md button-primary">
                                 Download CV
                             </button>
-                        </div>
+                        </Container>
                     </div>
                 </div>
             </div>
             <div class="section-resume section-resume-1 my-resume">
-                <div class="container">
+                <Container>
                     <div class="box">
-                        <div class="row row-0">
-                            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 bg-primary">
+                        <Row nogutter>
+                            <Col xs={12} sm={12} md={12} lg={12} xl={6} className="bg-primary">
                                 <div class="inner">
                                     <div class="resume-row resume-dark">
-                                        <h4 class="resume-title">Podcast</h4>
+                                        <h4 class="resume-title">Resume</h4>
                                         <ul>
                                             <li>
                                                 <div class="resume-item">
                                                     <div class="resume-head">
+                                                        <span class="resume-icon ti-pencil-alt"></span>
+                                                        <h5>Yoga Club Coach</h5>
+                                                        <h6>
+                                                            New York <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-heart"></span>
+                                                        <h5>Neo Club Coach</h5>
+                                                        <h6>
+                                                            New York <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
                                                         <span class="resume-icon ti-paint-bucket"></span>
-                                                        <h5>Fitness Podcast</h5>
+                                                        <h5>Fitness Trainer</h5>
                                                         <h6>
-                                                            NC, United States <small>(2016-2018)</small>
+                                                            New York <small>(2016-2018)</small>
                                                         </h6>
                                                     </div>
-                                                    <div class="resume-body"></div>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="resume-item">
-                                                    <div class="resume-head">
-                                                        <span class="resume-icon ti-cup"></span>
-                                                        <h5>Sport Podcast</h5>
-                                                        <h6>
-                                                            NC, United States <small>(2016-2018)</small>
-                                                        </h6>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
                                                     </div>
-                                                    <div class="resume-body"></div>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="resume-item">
-                                                    <div class="resume-head">
-                                                        <span class="resume-icon ti-ruler-alt-2"></span>
-                                                        <h5>New Podcast</h5>
-                                                        <h6>
-                                                            NC, United States <small>(2016-2018)</small>
-                                                        </h6>
-                                                    </div>
-                                                    <div class="resume-body"></div>
                                                 </div>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            </Col>
+                            <Col xs={12} sm={12} md={12} lg={12} xl={6}>
                                 <div class="inner">
                                     <div class="resume-row resume-light">
                                         <h4 class="resume-title">Experience</h4>
@@ -119,10 +126,10 @@ function About() {
                                         </ul>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </Col>
+                        </Row>
                     </div>
-                </div>
+                </Container>
             </div>
         </section>
     );
