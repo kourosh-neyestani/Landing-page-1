019c4779d6870203695e95002dcdb84daad9706a
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-12 11:58:35 +0430

    minor edit

--- src/scenes/Home/About.js
+++ src/scenes/Home/About.js
@@ -1,8 +1,131 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function About() {
-    return <section></section>;
+    return (
+        <section class="section section-about section-about-1 pp-scrollable">
+            <div class="full-screen">
+                <div class="display-center">
+                    <div class="section-title-1">
+                        <div class="container">
+                            <div class="my-avatar">
+                                <img src="assets/images/people/1.jpg" alt="My Avatar" />
+                            </div>
+                            <h2>I'am Miranda Smith.</h2>
+                            <h3>Your new Yoga Coach.</h3>
+                            <p>I'am a fitness coach base in New York since 2008, and i'am very passionate and dedicated to my work.</p>
+                            <button type="button" class="button button-md button-primary">
+                                Download CV
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-resume section-resume-1 my-resume">
+                <div class="container">
+                    <div class="box">
+                        <div class="row row-0">
+                            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 bg-primary">
+                                <div class="inner">
+                                    <div class="resume-row resume-dark">
+                                        <h4 class="resume-title">Podcast</h4>
+                                        <ul>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-paint-bucket"></span>
+                                                        <h5>Fitness Podcast</h5>
+                                                        <h6>
+                                                            NC, United States <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body"></div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-cup"></span>
+                                                        <h5>Sport Podcast</h5>
+                                                        <h6>
+                                                            NC, United States <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body"></div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-ruler-alt-2"></span>
+                                                        <h5>New Podcast</h5>
+                                                        <h6>
+                                                            NC, United States <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body"></div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                                <div class="inner">
+                                    <div class="resume-row resume-light">
+                                        <h4 class="resume-title">Experience</h4>
+                                        <ul>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-pencil-alt"></span>
+                                                        <h5>Yoga Club Coach</h5>
+                                                        <h6>
+                                                            New York <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-heart"></span>
+                                                        <h5>Neo Club Coach</h5>
+                                                        <h6>
+                                                            New York <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="resume-item">
+                                                    <div class="resume-head">
+                                                        <span class="resume-icon ti-paint-bucket"></span>
+                                                        <h5>Fitness Trainer</h5>
+                                                        <h6>
+                                                            New York <small>(2016-2018)</small>
+                                                        </h6>
+                                                    </div>
+                                                    <div class="resume-body">
+                                                        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default About;
