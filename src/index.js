2af749f4afb0f678cf4dd1bbc36f8ff2d2459449
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-19 17:56:26 +0430

    remove Switch from root

--- src/index.js
+++ src/index.js
@@ -17,26 +17,24 @@ const Root = () => {
     return (
         <Router>
             <div>
-                <Switch>
-                    <Route exact path="/">
-                        <Demo />
-                    </Route>
-                    <Route path="/home-1">
-                        <Home />
-                    </Route>
-                    <Route path="/home-2">
-                        <Home2 />
-                    </Route>
-                    <Route path="/home-3">
-                        <Home3 />
-                    </Route>
-                    <Route path="/home-4">
-                        <Home4 />
-                    </Route>
-                    <Route path="/home-5">
-                        <Home5 />
-                    </Route>
-                </Switch>
+                <Route exact path="/">
+                    <Demo />
+                </Route>
+                <Route path="/home-1">
+                    <Home />
+                </Route>
+                <Route path="/home-2">
+                    <Home2 />
+                </Route>
+                <Route path="/home-3">
+                    <Home3 />
+                </Route>
+                <Route path="/home-4">
+                    <Home4 />
+                </Route>
+                <Route path="/home-5">
+                    <Home5 />
+                </Route>
             </div>
         </Router>
     );
