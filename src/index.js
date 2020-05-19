1002014197297e0f1afe7a8eee19f4da3292d697
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-19 18:19:28 +0430

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, HashRouter, Link } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+
+// Stylesheet
+import "./index.scss";
 
 // Stylesheet
 import "./index.scss";
@@ -16,27 +19,45 @@ import Home5 from "./scenes/Home5";
 const Root = () => {
     return (
         <Router>
-            <div>
-                <Route exact path="/">
-                    <Demo />
-                </Route>
-                <Route path="/home-1">
-                    <Home />
-                </Route>
-                <Route path="/home-2">
-                    <Home2 />
-                </Route>
-                <Route path="/home-3">
-                    <Home3 />
-                </Route>
-                <Route path="/home-4">
-                    <Home4 />
-                </Route>
-                <Route path="/home-5">
-                    <Home5 />
-                </Route>
-            </div>
+            <Route exact path="/">
+                <Link to="/ali">ali</Link><hr />
+                <Link to="/home-1">h</Link><hr />
+                <Link to="/home-2">2</Link><hr />
+                <Link to="/home-3">3</Link><hr />
+                <Link to="/home-4">4</Link><hr />
+                <Link to="/home-5">5</Link><hr />
+                <Link to="/demo">demo</Link><hr />
+            </Route>
+            <Route path="/demo">
+                <Demo />
+            </Route>
+            <Route path="/ali">
+                <Ali />
+            </Route>
+            <Route path="/home-1">
+                <Home />
+            </Route>
+            <Route path="/home-3">
+                <Home3 />
+            </Route>
+            <Route path="/home-2">
+                <Home2 />
+            </Route>
+            <Route path="/home-4">
+                <Home4 />
+            </Route>
+            <Route path="/home-5">
+                <Home5 />
+            </Route>
         </Router>
+    );
+};
+
+const Ali = () => {
+    return (
+        <div>
+            <h1>Ali</h1>
+        </div>
     );
 };
 
