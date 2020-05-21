8a764ed92463d76d5f336e5ffe04754e56197362
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-21 17:58:29 +0430

    update error 404

--- src/components/common/Error404.js
+++ src/components/common/Error404.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function error404() {
     return (
-        <section className="section section-error-404">
+        <section className="section section-error-404 overlay-image">
             <div className="display-center">
                 <div className="error-content text-center">
                     <h1>404!</h1>
                     <h3>Page not found</h3>
-                    <span>The page you were looking for could not be found.</span>
+                    <p>The page you were looking for could not be found.</p>
+                    <Link to="/home-1">
+                        <button className="button button-lg button-primary">
+                            <span className="wave"></span>
+                            <span className="text">BACK TO HOMEPAGE</span>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </section>
